5ce32ee4bb4962bf29c3e778958222de228907a8
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-02-04 23:59:03 +0700

    layout finish

---
+++ app/(root)/my-training-records/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyTrainingRecords() {
+  return <div>MyTrainingRecords</div>;
+}
+
+export default MyTrainingRecords;
---
+++ app/(root)/upload-certificate/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UploadCertificate() {
+  return <div>UploadCertificate</div>;
+}
+
+export default UploadCertificate;
