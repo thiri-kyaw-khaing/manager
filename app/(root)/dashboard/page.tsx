63d14b1d1655b83214801ca5831bff386703a523
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-02-05 21:20:52 +0700

    dashboard card

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,5 +1,36 @@
-function DashboardPage() {
-  return <div>DashboardPage</div>;
-}
+import { BookOpenIcon, LineChartIcon, UsersIcon } from "lucide-react";
+import PageHeader from "@/components/dashboard/pageHeader";
+import DashboardCard from "@/components/dashboard/dashboardCard";
 
-export default DashboardPage;
+export default function DashboardPage() {
+  return (
+    <div className="m-6 space-y-4">
+      <PageHeader
+        title="Manager Dashboard"
+        subtitle="Department overview and personal training management"
+      />
+      <div className="flex flex-wrap gap-10">
+        <DashboardCard
+          icon={<UsersIcon className="w-6 h-6" />}
+          percentChange="+12%"
+          count={245}
+          description="Department Staff"
+        />
+
+        <DashboardCard
+          icon={<BookOpenIcon className="w-6 h-6" />}
+          percentChange="+8%"
+          count={120}
+          description="Active Training"
+        />
+
+        <DashboardCard
+          icon={<LineChartIcon className="w-6 h-6" />}
+          percentChange="+5%"
+          count={32}
+          description="Avg Training Hours"
+        />
+      </div>
+    </div>
+  );
+}
